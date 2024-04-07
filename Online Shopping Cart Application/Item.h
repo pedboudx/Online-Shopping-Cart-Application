2f076137920c90eb0ab7bdbba1372c779643afea
Aadesh Karial <saidaadesh@gmail.com>
2024-04-06 21:54:12 -0400

    Update Item.h

--- Online Shopping Cart Application/Item.h
+++ Online Shopping Cart Application/Item.h
@@ -1,62 +1,69 @@
 #pragma once
-#include <iostream>
-#include <string>
+#include <iostream> 
+#include <string>   
 using namespace std;
 
+// Creating Item class
 class Item
 {
 private:
-	string name;
-	double price;
-	string description;
+   string name;        // Name of the item
+   double price;       // Price of the item
+   string description; // Description of the item
 
 public:
-	Item() //default constructor
-	{
-		name = "";
-		price = 0;
-		description = "";
-	}
+   // Default constructor initializing member variables
+   Item()
+   {
+       name = "";
+       price = 0;
+       description = "";
+   }
 
-	Item(string _name, double _price, string _description) // values assigning constructor
-	{
-		name = _name;
-		price = _price;
-		description = _description;
-	}
+   // Overloaded Constructor
+   Item(string _name, double _price, string _description)
+   {
+       name = _name;
+       price = _price;
+       description = _description;
+   }
 
-    //Accessors
-	string getDescription()
-	{
-		return description;
-	}
+   // Getter function to retrieve the description of the item
+   string getDescription()
+   {
+       return description;
+   }
 
-	string getName()
-	{
-		return name;
-	}
+   // Setter function to set the description of the item
+   void setDescription(string _description)
+   {
+       description = _description;
+   }
 
-	double getPrice()
-	{
-		return price;
-	}
+   // Getter function to retrieve the name of the item
+   string getName()
+   {
+       return name;
+   }
 
-    //Mutators
-	void setDescription(string _description)
-	{
-		description = _description;
-	}
+   // Getter function to retrieve the price of the item
+   double getPrice()
+   {
+       return price;
+   }
 
-	void setName(string _name)
-	{
-		name = _name;
-	}
+   // Setter function to set the name of the item
+   void setName(string _name)
+   {
+       name = _name;
+   }
 
-	void setPrice(double _price)
-	{
-		price = _price;
-	}
+   // Setter function to set the price of the item
+   void setPrice(double _price)
+   {
+       price = _price;
+   }
 
-    //virtual print function
-	virtual void print() = 0;
+   // Pure virtual function to be implemented by derived classes for printing item details
+   virtual void print() = 0;
 };
